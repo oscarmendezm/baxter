067d11a197baf2206150f24b148e0179dc400e2e
rhysbryant1 <rhysbryant@hotmail.co.uk>
2015-12-11 09:56:26 +0000

    Able to point gripper down

--- tennis_ball_sorter/tennis_ball_sorter.py
+++ tennis_ball_sorter/tennis_ball_sorter.py
@@ -5,7 +5,6 @@ __author__ = 'rhys'
 # to move these into a bin/defined location, also on the table.
 
 # Imports
-import freenect
 import numpy as np
 import cv
 from cv_bridge import CvBridge, CvBridgeError
@@ -17,7 +16,7 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-import octomap
+
 
 
 class Initialise:
@@ -44,87 +43,77 @@ class Initialise:
 
         # Create this DisplayTrajectory publisher which is used below to publish trajectories for RVIZ to visualize.
         self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                            moveit_msgs.msg.DisplayTrajectory)
+                                                            moveit_msgs.msg.DisplayTrajectory,
+                                                            queue_size=10)
+                                                            
+        ## Getting Basic Information
+	  	## ^^^^^^^^^^^^^^^^^^^^^^^^^
+	  	##
+	  	## We can get the name of the reference frame for this robot
+        print "============ Reference frame Left: %s" % self.left_arm.get_planning_frame()
+        print "============ Reference frame Rght: %s" % self.right_arm.get_planning_frame()
+        
+        ## We can also print the name of the end-effector link for this group
+        print "============ End Effector Left: %s" % self.left_arm.get_end_effector_link()
+        print "============ End Effector Rght: %s" % self.right_arm.get_end_effector_link()
+        
+        ## We can get a list of all the groups in the robot
+        print "============ Robot Groups:"
+        print self.robot.get_group_names()
+        
+        ## We can get a list of all the groups in the robot
+        print "============ Robot GroupsPoses:"
+        print self.left_arm.get_current_pose()
+        print self.right_arm.get_current_pose()
 
+        ## Sometimes for debugging it is useful to print the entire state of the
+        ## robot.
+        print "============ Printing robot state"
+        print self.robot.get_current_state()
+        print "============"                                                   
+                                                            
         # Wait for RVIZ to initialize. This sleep is ONLY to allow Rviz to come up.
-        print "============ Waiting for RVIZ..."
-        rospy.sleep(10)
+        #print "============ Waiting for RVIZ..."
+        #rospy.sleep(10)
+        
+        # Add a table to the environment
+        table = PoseStamped()
+        table.header.frame_id = self.robot.get_planning_frame()
+        
+        table.pose.position.x = 
 
     def test_movement(self):
         """
         Instruct Baxters arms to complete a simple motion task to ensure that initialisation was successful.
         """
 
-        # Set a plan for each arm group so that moveit! can plan the movement needed.
-        print "============ Generating right_arm plan"
-        pose_target = create_pose_target(1.0, 0.7, -0.05, 1.1)
-        self.right_arm.set_pose_target(pose_target)
-        right_arm_plan = self.right_arm.plan()
-
         print "============ Generating left_arm plan"
-        pose_target = create_pose_target(1.0, 0.7, -0.05, 1.1)
+        pose_target = create_pose_target(0.000000,		# Ww
+        								 0.000000,		# Wx
+        								 1.000000,		# Wy	
+        								 0.000000,		# Wz
+        								 0.8, 0.0, 0.0)	# X, Y, Z
+        self.left_arm.set_goal_tolerance(0.01);
+        self.left_arm.set_planner_id("RRTConnectkConfigDefault");
         self.left_arm.set_pose_target(pose_target)
         left_arm_plan = self.left_arm.plan()
 
-        # Wait for Rviz to plan the movement for each group
-        print "============ Waiting while plans are visualized..."
-        rospy.sleep(5)
-
         # Execute the movement
         print "============ Executing plans"
-        right_arm_plan.go()
-        left_arm_plan.go()
-
-    def kinect_setup(self):
-        """
-        Function to setup the kinect topic. As well as the Octomap
-        :return:
-        """
+        self.left_arm.go()
 
 
-        depth_image, _ = freenect.sync_get_depth()
-        depth_image = depth_image.astype(np.uint8)
-
-
-        tree = octomap.OcTree(depth_image)
-        view_free = False
-
-        itr = tree.begin_tree()
-        root_size = itr.getSize()
-        op =[]
-        fp = []
-        so = []
-        sf = []
-
-        for i in itr:
-            if i.isLeaf():
-                so.append(i.getSize() / root_size)
-                op.append(i.getCoordinate())
-            else:
-                if view_free:
-                    sf.append(i.getSize() / root_size)
-                    fp.append(i.getCoordinate())
-
-        op = zip(*op)
-        fp = zip(*fp)
-        points3d(op[0], op[1], op[2], so, opacity=1.0, mode='cube',
-                 color=(0, 0, 1), scale_mode='scalar', scale_factor=root_size)
-
-        if view_free:
-            points3d(fp[0], fp[1], fp[2], sf, opacity=0.3, mode='cube',
-                     color=(0, 1, 0), scale_mode='scalar', scale_factor=root_size)
-        show()
-
-
-
-def create_pose_target(w, x, y, z):
+def create_pose_target(Ww, Wx, Wy, Wz, x, y, z):
     """
     Take in an  w, x, y and z values to create a pose target
     :return: pose_target - a target for a group of joints to move to.
     """
 
     pose_target = geometry_msgs.msg.Pose()
-    pose_target.orientation.w = w
+    pose_target.orientation.w = Ww
+    pose_target.orientation.x = Wx
+    pose_target.orientation.y = Wy
+    pose_target.orientation.z = Wz
     pose_target.position.x = x
     pose_target.position.y = y
     pose_target.position.z = z
@@ -136,10 +125,8 @@ def main():
 
     # Run the Initialise class to setup Baxter and the environment
     baxter = Initialise()
+    baxter.test_movement()
 
-    while 1:
-        # Run a test move to show that the script is running correctly
-        baxter.kinect_setup()
-
+   
 if __name__ == '__main__':
     main()
