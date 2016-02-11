7cf37fad98cce34daa1ef6a3826a450ee08aea5d
rhysbryant1 <rhysbryant@hotmail.co.uk>
2016-02-11 16:00:34 +0000

    Added functionality to threshold the image.
    By thresholding the image, a plain can be specified for object recognition

--- tennis_ball_sorter/kinect_publisher.py
+++ tennis_ball_sorter/kinect_publisher.py
@@ -6,6 +6,7 @@ from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
 
+
 class image_converter:
     def __init__(self):
         self.bridge = CvBridge()
@@ -14,9 +15,11 @@ class image_converter:
     def callback(self, data):
         depth_image = self.bridge.imgmsg_to_cv2(data, "16UC1")
         depth_array = np.array(depth_image, dtype=np.float32)
-        cv2.normalize(depth_array, depth_array, 0, 255, cv2.NORM_MINMAX)
+        cv2.normalize(depth_array, depth_array, 0, 1, cv2.NORM_MINMAX)
 
-        cv2.imshow("Depth", depth_array)
+        ret, bin_img = cv2.threshold(depth_array, 0.5, 1, cv2.THRESH_BINARY_INV)
+
+        cv2.imshow("Depth", bin_img)
         cv2.waitKey(3)
 
 def main(args):
