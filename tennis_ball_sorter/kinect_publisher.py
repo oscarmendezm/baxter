9b9b6a0b6299c7a666f62b28606b153c74734d24
rhysbryant1 <rhysbryant@hotmail.co.uk>
2016-02-15 18:58:09 +0000

    Working code to place box around every object seen

--- tennis_ball_sorter/kinect_publisher.py
+++ tennis_ball_sorter/kinect_publisher.py
@@ -26,12 +26,21 @@ class Object_Detection:
         depth_array = np.array(depth_image, dtype=np.float32)
         cv2.normalize(depth_array, depth_array, 0, 1, cv2.NORM_MINMAX)
 
+        # Pass the image to to object detection function
+        self.object_detection(depth_array)
+
+    def object_detection(self, depth_array):
+        """
+        Function to detect objects from the depth image given
+        :return:
+        """
+
         # Perform thresholding on the image to remove all objects behind a plain
         ret, bin_img = cv2.threshold(depth_array, 0.3, 1, cv2.THRESH_BINARY_INV)
 
         # Erode the image a few times in order to separate close objects
         element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-        err_img = cv2.erode(bin_img, element, iterations=10)
+        err_img = cv2.erode(bin_img, element, iterations=20)
 
         # Create a new array of type uint8 for the findContours function
         con_img = np.array(err_img, dtype=np.uint8)
@@ -41,12 +50,12 @@ class Object_Detection:
         cv2.drawContours(con_img, contours, -1, (128, 255, 0), 3)
 
         # Use the contours to draw a rectangle around the object
-        x, y, w, h = cv2.boundingRect(contours)
-        cv2.rectangle(con_img, (x, y), ((x+w), (y+h)), (255, 0, 0), thickness=5, lineType=8, shift=0)
+        for x in range(0, len(contours)):
+            x, y, w, h = cv2.boundingRect(contours[x])
+            cv2.rectangle(con_img, (x, y), ((x+w), (y+h)), (255, 0, 127), thickness=5, lineType=8, shift=0)
 
         # Show the produced images
         cv2.imshow('Contours', con_img)
-        cv2.imshow("Erode", err_img)
         cv2.imshow("Depth", bin_img)
         cv2.waitKey(3)
 
