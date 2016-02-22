547a82001a6f4ff7f7da5befd04d3a969cb1379e
rhysbryant1 <rhysbryant@hotmail.co.uk>
2016-02-22 11:23:09 +0000

    In the process of rewritting for functionality

--- tennis_ball_sorter/move.py
+++ tennis_ball_sorter/move.py
@@ -1,4 +1,4 @@
-__author__ = 'rhys'
+__author__ = 'Rhys Bryant'
 
 # Imports
 import numpy as np
@@ -15,13 +15,23 @@ import moveit_msgs.msg
 import geometry_msgs.msg
 
 
-class Robot:
+class Control:
     def __init__(self):
         """
-        Set up all of the subscriptions and objects to control the robot
-        :return: 
-        """  
-        
+        Setup ROS communication between the Kinect and Baxter
+        :return:
+        """
+
+        # Initialise variables
+        self.rgb_img = None
+        self.depth_img = None
+        self.marker_box = None
+        self.marker_center_x = None
+        self.marker_center_y = None
+        self.marker_depth = None
+
+        self.bridge = CvBridge()
+
         # First initialize moveit_commander and rospy.
         print "============ Initialising Baxter"
         moveit_commander.roscpp_initialize(sys.argv)
@@ -36,187 +46,192 @@ class Robot:
         self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                             moveit_msgs.msg.DisplayTrajectory,
                                                             queue_size=10)
-        
+
         # We can get a list of all the groups in the robot
         print "============ Robot GroupsPoses:"
         print self.left_arm.get_current_pose()
-        #print self.right_arm.get_current_pose()
-        
-    def move_to_calibrate(self, test):
-        """
-        Instruct baxters arm to move to a set point for calibration
-        """
-        
-        if test is 1:
-            print "============ Generating left_arm plan"
-            pose_target = create_pose_target(0.251457698541,		# Ww
-                                             0.132660864689,		# Wx
-                                             -0.937502362532,		# Wy	
-                                             0.200647554364,		# Wz
-                                             0.453869796845, 		# X
-                                             -0.0301870563505, 		# Y
-                                             0.150496092849)		# Z
 
-            
-        else:
-            pose_target = create_pose_target(0.251457698541,		# Ww
-                                             0.132660864689,		# Wx
-                                             -0.937502362532,		# Wy	
-                                             0.200647554364,		# Wz
-                                             0.553869796845, 		# X
-                                             -0.1301870563505, 		# Y
-                                             0.250496092849)		# Z
-                                             
-        self.left_arm.set_goal_tolerance(0.01);
-        self.left_arm.set_planner_id("RRTConnectkConfigDefault");
-        self.left_arm.set_pose_target(pose_target)
-        left_arm_plan = self.left_arm.plan()
-        
-        # Execute the movement
-        print "============ Executing plans"
-        self.left_arm.go()
-
-      
-class Calibration:
-    def __init__(self):
-        """
-        Setup subscribers and publishers for vision 
-        """
-        self.yellow_img = None
-        self.eroded_yellow_img = None
-        self.rgb_img = None
-        self.depth_img = None
-        self.contour = None
-        self.center_x = 0
-        self.center_y = 0
-        self.bridge = CvBridge()
-        
+        # Setup the subscribers and publishers
         self.rgb_sub = rospy.Subscriber("camera/rgb/image_color", Image, self.callback_rgb)
         self.depth_sub = rospy.Subscriber("camera/depth/image", Image, self.callback_depth)
-        self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-        													moveit_msgs.msg.DisplayTrajectory,
-        													queue_size=5)      
-        
+        self.display_trajectory_publisher = rospy.Publiser('/move_group/display_planned_path',
+                                                           moveit_msgs.msg.DisplayTrajectory,
+                                                           queue_size=5)
+
     def callback_rgb(self, data):
         """
-        Function to handle data arriving for RGB
-        :param data: 
-        :return: 
+        Function to handle the arrival of RGB data
+        :param data:
+        :return:
         """
-        # Convert to usable format
+
+        # Convert the data to a usable format
         self.rgb_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        
-        # View only Yellow to find point on gripper
-        hsv = cv2.cvtColor(self.rgb_img, cv2.COLOR_RGB2HSV)
-        lower = np.array([20, 100, 100], dtype=np.uint8)
-        upper = np.array([100, 255, 255], dtype=np.uint8)
-        mask = cv2.inRange(hsv, lower, upper)
-        self.yellow_img = cv2.bitwise_and(self.rgb_img, self.rgb_img, mask=mask)
-        
-        # Erode the image a few times in order to separate close objects
-        element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-        self.eroded_yellow_img = cv2.erode(self.yellow_img, element, iterations=2)  	
-        
-        # Determine the location of the yellow circle in RGB 
-        self.yellow_circle_detection()
-        
+
     def callback_depth(self, data):
         """
-        Function to handle data arriving for depth
-        :param data: 
-        :return: 
+        Function to handle the arrival of depth data
+        :param data:
+        :return:
         """
-        # Convert the received image to a CV image and normalise it to grayscale
+
+        # Convert the data to a usable format
         depth = self.bridge.imgmsg_to_cv2(data, "16UC1")
         self.depth_img = np.array(depth, dtype=np.float32)
         cv2.normalize(self.depth_img, self.depth_img, 0, 1, cv2.NORM_MINMAX)
 
-        # Add a bit to the x value to account for the off center camera
-        self.center_x += 80
-        
-        # Print the center of the circle on the depth image
-        cv2.rectangle(self.depth_img, (self.center_x, self.center_y), ((self.center_x+1), (self.center_y+1)), (255, 0, 127), thickness=5, lineType=8, shift=0)
-        cv2.imshow("Depth", self.depth_img)
-        cv2.waitKey(5)
-        
-        # Determine the depth of this point
-        print self.depth_img[self.center_x][self.center_y]
-        print self.center_x + 80
-        print self.center_y
+    def calibrate_main(self):
+        """
+        Function to complete the calibration between the Kinect and Baxter coordinate frames
+        :return:
+        """
+        kinect_coordinate = [None * 3]
+        points_detected = False
 
-    def yellow_circle_detection(self):
+        # Loop through 3 points in order to use Affine transformation
+        for point in range(0, 2):
+            self.move_to_calibrate(point)
+            while not points_detected:
+                points_detected = self.detect_calibration_marker()
+
+    def move_to_calibrate(self, p):
         """
-        Identify objects on depth
-        :return: 
+        Move a Baxter arm to the passed in set calibration point
+        :return:
         """
-        gray_image = cv2.cvtColor(self.eroded_yellow_img, cv2.COLOR_BGR2GRAY)
-        
+
+        print "============ Moving to calibration point " + p
+
+        # Determine the point to be moved to
+        if p is 0:
+            pose_target = self.create_pose_target(0.251457698541,		# Ww
+                                                  0.132660864689,		# Wx
+                                                  -0.937502362532,		# Wy
+                                                  0.200647554364,		# Wz
+                                                  0.453869796845, 		# X
+                                                  -0.0301870563505, 	# Y
+                                                  0.150496092849)		# Z
+
+        elif p is 1:
+            pose_target = self.create_pose_target(0.251457698541,		# Ww
+                                                  0.132660864689,		# Wx
+                                                  -0.937502362532,		# Wy
+                                                  0.200647554364,		# Wz
+                                                  0.553869796845, 		# X
+                                                  -0.1301870563505, 	# Y
+                                                  0.250496092849)		# Z
+
+        elif p is 2:
+            pose_target = self.create_pose_target(0.251457698541,		# Ww
+                                                  0.132660864689,		# Wx
+                                                  -0.937502362532,		# Wy
+                                                  0.200647554364,		# Wz
+                                                  0.553869796845, 		# X
+                                                  -0.1301870563505, 	# Y
+                                                  0.250496092849)		# Z
+
+        self.left_arm.set_goal_tolerance(0.01)
+        self.left_arm.set_planner_id("RRTConnectkConfigDefault")
+        self.left_arm.set_pose_target(pose_target)
+        left_arm_plan = self.left_arm.plan()
+        self.left_arm.go()
+
+    def detect_calibration_marker(self):
+        """
+        Function to detect the marker on
+        :return:
+        """
+
+        # View only the yellow colours in the image
+        hsv = cv2.cvtColor(self.rgb_img, cv2.COLOR_RGB2HSV)
+        lower = np.array([20, 100, 100], dtype=np.uint8)
+        upper = np.array([100, 255, 255], dtype=np.uint8)
+        mask = cv2.inRange(hsv, lower, upper)
+        yellow_img = cv2.bitwise_and(self.rgb_img, self.rgb_img, mask=mask)
+
+        # Erode the image a few times in order to separate close objects
+        element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+        eroded_yellow_img = cv2.erode(yellow_img, element, iterations=2)
+
+        gray_image = cv2.cvtColor(eroded_yellow_img, cv2.COLOR_BGR2GRAY)
+
         # Perform thresholding on the image to remove all objects behind a plain
         ret, bin_img = cv2.threshold(gray_image, 0.3, 1, cv2.THRESH_BINARY_INV)
-        
+
         # Create a new array of type uint8 for the findContours function
         con_img = np.array(gray_image, dtype=np.uint8)
-        
+
         # Find the contours of the image and then draw them on
         contours, hierarchy = cv2.findContours(con_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cv2.drawContours(con_img, contours, -1, (128, 255, 0), 3)
-        
+
         try:
-		    # Find the largest rectangle and discard the others
-		    for i in range(0, len(contours)):
-		    	x, y, w, h = cv2.boundingRect(contours[i])
-		    	area = w * h
-		    	if i is 0:
-		    		current_max = area
-		    		max_x = i
-		    	else:
-		    		if area > current_max:
-		    			current_max = area
-		    			max_x = i
-		    			
-		    # Display the largest box
-		    x, y, w, h = cv2.boundingRect(contours[max_x])
-		    self.contour = contours[max_x]
-		    cv2.rectangle(self.rgb_img, (x, y), ((x+w), (y+h)), (255, 0, 127), thickness=5, lineType=8, shift=0)
+            # Find the largest rectangle and discard the others
+            for i in range(0, len(contours)):
+                x, y, w, h = cv2.boundingRect(contours[i])
+                area = w * h
+                if i is 0:
+                    current_max = area
+                    max_x = i
+                else:
+                    if area > current_max:
+                        current_max = area
+                        max_x = i
+
+            # Display the largest box
+            x, y, w, h = cv2.boundingRect(contours[max_x])
+            self.marker_box = contours[max_x]
+            cv2.rectangle(self.rgb_img, (x, y), ((x+w), (y+h)), (255, 0, 127), thickness=5, lineType=8, shift=0)
         except:
             print "No Circle found"
 
         # Find the Centre of the largest box
-        self.center_x = int(x + (0.5 * w))
-        self.center_y = int(y + (0.5 * h))
- 
-        cv2.imshow("Calibration", self.rgb_img)
-        cv2.waitKey(5)
+        self.marker_center_x = int(x + (0.5 * w))
+        self.marker_center_y = int(y + (0.5 * h))
+        
+        # Using the center of the marker, find the depth value
+        self.marker_depth = self.depth_img[self.center_x][self.center_y]
+        
+        # Check if the function returned valid data and return the answer
+        if self.marker_center_x or self.marker_center_y or self.marker_depth is not None:
+            return True
+        else:
+            return False
 
-			
-def create_pose_target(Ww, Wx, Wy, Wz, x, y, z):
-    """
-    Take in an  w, x, y and z values to create a pose target
-    :return: pose_target - a target for a group of joints to move to.
-    """
+    def create_pose_target(self, Ww, Wx, Wy, Wz, x, y, z):
+        """
+        Take in an  w, x, y and z values to create a pose target
+        :return: pose_target - a target for a group of joints to move to.
+        """
 
-    pose_target = geometry_msgs.msg.Pose()
-    pose_target.orientation.w = Ww
-    pose_target.orientation.x = Wx
-    pose_target.orientation.y = Wy
-    pose_target.orientation.z = Wz
-    pose_target.position.x = x
-    pose_target.position.y = y
-    pose_target.position.z = z
+        pose_target = geometry_msgs.msg.Pose()
+        pose_target.orientation.w = Ww
+        pose_target.orientation.x = Wx
+        pose_target.orientation.y = Wy
+        pose_target.orientation.z = Wz
+        pose_target.position.x = x
+        pose_target.position.y = y
+        pose_target.position.z = z
 
-    return pose_target
+        return pose_target
 
 
 def main():
-    
-    # Initialise node
-    rospy.init_node('can_node',anonymous=True)
-    robot = Robot()
-    robot.move_to_calibrate(2)
-    calibration = Calibration()
-    
-    # Keep these images updating        
-    rospy.spin()  
-   
+    """
+    Main control for operations
+    :return:
+    """
+    # Initialise Node
+    rospy.init_node('movement_node', anonymous=True)
+
+    # Initialise the Control class to complete all operations on Baxter
+    robot = Control()
+
+    # Start the calibration process to link Baxter and Kinect Coordinates
+    robot.calibrate_main()
+
+    # Keep the topics updating
+    rospy.spin()
+
 if __name__ == '__main__':
     main()
