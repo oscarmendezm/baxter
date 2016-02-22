b46d3bd0b955ed895b3af20db0cb4cbf880f6928
rhysbryant1 <rhysbryant@hotmail.co.uk>
2016-02-22 17:26:35 +0000

    Move to 3 points and get Kinect coordinates

--- tennis_ball_sorter/move.py
+++ tennis_ball_sorter/move.py
@@ -13,6 +13,7 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+import time
 
 
 class Control:
@@ -35,8 +36,7 @@ class Control:
         # First initialize moveit_commander and rospy.
         print "============ Initialising Baxter"
         moveit_commander.roscpp_initialize(sys.argv)
-        rospy.init_node('can_node',
-                        anonymous=True)
+        rospy.init_node('can_node', anonymous=True)
 
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
@@ -54,7 +54,7 @@ class Control:
         # Setup the subscribers and publishers
         self.rgb_sub = rospy.Subscriber("camera/rgb/image_color", Image, self.callback_rgb)
         self.depth_sub = rospy.Subscriber("camera/depth/image", Image, self.callback_depth)
-        self.display_trajectory_publisher = rospy.Publiser('/move_group/display_planned_path',
+        self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                            moveit_msgs.msg.DisplayTrajectory,
                                                            queue_size=5)
 
@@ -78,21 +78,32 @@ class Control:
         # Convert the data to a usable format
         depth = self.bridge.imgmsg_to_cv2(data, "16UC1")
         self.depth_img = np.array(depth, dtype=np.float32)
-        cv2.normalize(self.depth_img, self.depth_img, 0, 1, cv2.NORM_MINMAX)
+        cv2.normalize(self.depth_img, self.depth_img, 0, 1, cv2.NORM_MINMAX)                
 
     def calibrate_main(self):
         """
         Function to complete the calibration between the Kinect and Baxter coordinate frames
         :return:
         """
-        kinect_coordinate = [None * 3]
+        kinect_coordinate = [None] * 3
         points_detected = False
 
         # Loop through 3 points in order to use Affine transformation
-        for point in range(0, 2):
-            self.move_to_calibrate(point)
+        for point in range(0, 3):
+            baxter_coordinate = self.move_to_calibrate(point)
+            
+            # Add small sleep time so the Kinect images can update
+            time.sleep(2)
             while not points_detected:
-                points_detected = self.detect_calibration_marker()
+            	if (self.rgb_img is not None) and (self.depth_img is not None):
+                    points_detected = self.detect_calibration_marker()
+                    if points_detected:
+                    	kinect_coordinate[point] = [float(self.marker_center_x),
+                    								float(self.marker_center_y),
+                    								float(self.marker_depth)]
+                        print "Kinect: " + str(kinect_coordinate[point])
+                        print "Baxter: " + str(baxter_coordinate)
+            points_detected = False
 
     def move_to_calibrate(self, p):
         """
@@ -100,48 +111,59 @@ class Control:
         :return:
         """
 
-        print "============ Moving to calibration point " + p
+        print '\n' + "============ Moving to calibration point " + str(p)
 
         # Determine the point to be moved to
         if p is 0:
-            pose_target = self.create_pose_target(0.251457698541,		# Ww
-                                                  0.132660864689,		# Wx
-                                                  -0.937502362532,		# Wy
-                                                  0.200647554364,		# Wz
-                                                  0.453869796845, 		# X
-                                                  -0.0301870563505, 	# Y
-                                                  0.150496092849)		# Z
+        	x = 0.605034870312
+        	y = -0.0912329615274
+        	z = 0.171568849531
+        	pose_target = self.create_pose_target(0.275136546309,		# Ww
+                                                  -0.0626316715375,		# Wx
+                                                  -0.959281393733,		# Wy
+                                                  -0.0125044889574,		# Wz
+                                                  x, 		            # X
+                                                  y, 					# Y
+                                                  z)					# Z
 
         elif p is 1:
-            pose_target = self.create_pose_target(0.251457698541,		# Ww
-                                                  0.132660864689,		# Wx
-                                                  -0.937502362532,		# Wy
-                                                  0.200647554364,		# Wz
-                                                  0.553869796845, 		# X
-                                                  -0.1301870563505, 	# Y
-                                                  0.250496092849)		# Z
+        	x = 0.409499392644
+        	y = 0.040785097559
+        	z = 0.114928857492
+        	pose_target = self.create_pose_target(0.36478892055,		# Ww
+                                                  0.241465467454,		# Wx
+                                                  -0.830279996407,		# Wy
+                                                  0.345338383383,		# Wz
+                                                  x, 					# X
+                                                  y, 	    			# Y
+                                                  z)					# Z
 
         elif p is 2:
-            pose_target = self.create_pose_target(0.251457698541,		# Ww
+        	x = 0.453869796845
+        	y = -0.0301870563505
+        	z = 0.150496092849
+        	pose_target = self.create_pose_target(0.251457698541,		# Ww
                                                   0.132660864689,		# Wx
                                                   -0.937502362532,		# Wy
                                                   0.200647554364,		# Wz
-                                                  0.553869796845, 		# X
-                                                  -0.1301870563505, 	# Y
-                                                  0.250496092849)		# Z
+                                                  x, 					# X
+                                                  y, 					# Y
+                                                  z)					# Z
 
         self.left_arm.set_goal_tolerance(0.01)
         self.left_arm.set_planner_id("RRTConnectkConfigDefault")
         self.left_arm.set_pose_target(pose_target)
         left_arm_plan = self.left_arm.plan()
         self.left_arm.go()
+        
+        return [x, y, z]
 
     def detect_calibration_marker(self):
         """
         Function to detect the marker on
         :return:
         """
-
+        
         # View only the yellow colours in the image
         hsv = cv2.cvtColor(self.rgb_img, cv2.COLOR_RGB2HSV)
         lower = np.array([20, 100, 100], dtype=np.uint8)
@@ -190,7 +212,7 @@ class Control:
         self.marker_center_y = int(y + (0.5 * h))
         
         # Using the center of the marker, find the depth value
-        self.marker_depth = self.depth_img[self.center_x][self.center_y]
+        self.marker_depth = self.depth_img[self.marker_center_x][self.marker_center_y]
         
         # Check if the function returned valid data and return the answer
         if self.marker_center_x or self.marker_center_y or self.marker_depth is not None:
@@ -221,8 +243,6 @@ def main():
     Main control for operations
     :return:
     """
-    # Initialise Node
-    rospy.init_node('movement_node', anonymous=True)
 
     # Initialise the Control class to complete all operations on Baxter
     robot = Control()
