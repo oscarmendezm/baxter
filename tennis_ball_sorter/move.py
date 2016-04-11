a0025490e765b749cf23642cc23c9fc951e0682c
rhysbryant1 <rhysbryant@hotmail.co.uk>
2016-04-11 12:25:20 +0100

    getting data from point cloud, rigid transform

--- tennis_ball_sorter/move.py
+++ tennis_ball_sorter/move.py
@@ -6,22 +6,24 @@ import cv
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import String
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2, PointField
+import sensor_msgs.point_cloud2 as pc2
 from baxter_interface import Navigator
+import sys
 import copy
 import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 import time
+import sensor_msgs.point_cloud2 as pc2
+import math
 from numpy import *
 from math import sqrt
 import scipy
 import math
 import sys
-from geometry_msgs.msg import PoseStamped, Pose
-from moveit_commander import MoveGroupCommander, PlanningSceneInterface
-from moveit_msgs.msg import PlanningScene, ObjectColor
+
 
 class Control:
     def __init__(self):
@@ -50,30 +52,6 @@ class Control:
         self.left_arm = moveit_commander.MoveGroupCommander("left_arm")
         self.right_arm = moveit_commander.MoveGroupCommander("right_arm")
         self.right_arm_navigator = Navigator('right')
-        
-        # Setup the table in the scene
-        scene = PlanningSceneInterface()
-        self.scene_pub = rospy.Publisher('/move_group/monitored_planning_scene', PlanningScene)
-        table_id = 'table'
-        scene.remove_world_object(table_id)
-        rospy.sleep(1)
-        table_ground = -0.2
-        table_size = [0.7, 1.4, 0.1]
-        
-        table_pose = PoseStamped()
-        table_pose.header.frame_id = self.robot.get_planning_frame()
-        table_pose.pose.position.x = 0.7
-        table_pose.pose.position.y = 0.0
-        table_pose.pose.position.z = table_ground + table_size[2] / 2.0
-        table_pose.pose.orientation.w = 1.0
-        scene.add_box(table_id, table_pose, table_size)
-        
-        # Create a dictionary to hold object colors
-        self.colors = dict()
-        self.setColor(table_id, 0.8, 0, 0, 1.0)
-        
-        self.sendColors()
-        
 
         self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                             moveit_msgs.msg.DisplayTrajectory,
@@ -87,43 +65,21 @@ class Control:
         # Setup the subscribers and publishers
         self.rgb_sub = rospy.Subscriber("camera/rgb/image_rect_color", Image, self.callback_rgb)
         self.depth_sub = rospy.Subscriber("camera/depth_registered/image_raw", Image, self.callback_depth)
+        self.points_sub = rospy.Subscriber("/camera/depth_registered/points", PointCloud2,  self.callback_points) 
         self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                            moveit_msgs.msg.DisplayTrajectory,
                                                            queue_size=5)
         self.screen_pub = rospy.Publisher('robot/xdisplay', Image, latch=True, queue_size=10)
-        
-    # Set the color of an object
-    def setColor(self, name, r, g, b, a = 0.9):
-        # Initialize a MoveIt color object
-        color = ObjectColor()
 
-        # Set the id to the name given as an argument
-        color.id = name
-
-        # Set the rgb and alpha values given as input
-        color.color.r = r
-        color.color.g = g
-        color.color.b = b
-        color.color.a = a
-
-        # Update the global color dictionary
-        self.colors[name] = color
-
-    # Actually send the colors to MoveIt!
-    def sendColors(self):
-        # Initialize a planning scene object
-        p = PlanningScene()
-
-        # Need to publish a planning scene diff        
-        p.is_diff = True
-
-        # Append the colors from the global color dictionary 
-        for color in self.colors.values():
-            p.object_colors.append(color)
-
-        # Publish the scene diff
-        self.scene_pub.publish(p)
-
+    def callback_points(self, data):
+		"""
+		Function to handle the arrival of pointcloud data
+		"""
+		
+		cloud = list(pc2.read_points(data, skip_nans=False, field_names=("x", "y", "z")))
+		cloud = np.resize(cloud, [640, 480, 3])
+		self.cloud = cloud
+		
     def callback_rgb(self, data):
         """
         Function to handle the arrival of RGB data
@@ -190,7 +146,7 @@ class Control:
         # Convert the data to a usable format
         depth = self.bridge.imgmsg_to_cv2(data, "16UC1")
         self.depth_img = np.array(depth, dtype=np.float32)
-        cv2.normalize(self.depth_img, self.depth_img, 0, 1, cv2.NORM_MINMAX)           
+        cv2.normalize(self.depth_img, self.depth_img, 0, 1, cv2.NORM_MINMAX)                
 
     def calibrate_main(self):
         """
@@ -217,7 +173,7 @@ class Control:
                     if (self.rgb_img is not None) and (self.depth_img is not None):
                         try:
                             points_detected = self.detect_calibration_marker()
-                            if self.marker_depth == 0.0 or self.marker_depth > 1:
+                            if self.marker_depth is 0.0:
                                 points_detected = False
                         except:
                             points_detected = False
@@ -225,24 +181,24 @@ class Control:
                             baxter_points.append([x, y, z])
                             kinect_points.append([float(self.marker_center_x),
                             					 float(self.marker_center_y), 
-                            					 float(self.marker_depth)])
+                            					 float(self.marker_center_z)])
                             print "Kinect: " + str(kinect_points[point])
                             print "Baxter: " + str(baxter_points[point])
                             point += 1
                             time.sleep(1)
                 points_detected = False
-                
-        output_log = open("output.txt", "w")
-        output_log.write("Baxter Points\n")
-        output_log.write(str(baxter_points))
-        output_log.write("\n\n\nKinect Points\n")
-        output_log.write(str(kinect_points))  
             
         kinect_points = np.asmatrix(kinect_points)
         baxter_points = np.asmatrix(baxter_points)
         
+        time.sleep(1)
+           
         self.R, self.t = self.calculate_transform(kinect_points, baxter_points)
         
+        print "\nTesting Result: \n"
+        print "Original Baxter point: \n" + str(baxter_points[0])
+        cal_bax_point = self.calculate_translated_point(kinect_points[0])
+        print "\n\nCalculated Baxter point: \n" + str(cal_bax_point)
         
     def calculate_transform(self, K, B):
         # Ransac parameters
@@ -335,7 +291,6 @@ class Control:
     	rmse = sqrt(err / n)
     	
     	return rmse
-        
         
 
     def move_to_remove(self):
@@ -431,17 +386,21 @@ class Control:
            
 
         # Find the Centre of the largest box
-        self.marker_center_x = int(x + (0.5 * w))
-        self.marker_center_y = int(y + (0.5 * h))
+        box_center_x = int(x + (0.5 * w))
+        box_center_y = int(y + (0.5 * h))
         
         # Using the center of the marker, find the depth value
-        self.marker_depth = self.depth_img[self.marker_center_x][self.marker_center_y]
+        # self.marker_depth = self.depth_img[self.marker_center_x][self.marker_center_y]
+        
+        self.marker_center_x = self.cloud[box_center_x][box_center_y][0]
+        self.marker_center_y = self.cloud[box_center_x][box_center_y][1]
+        self.marker_center_z = self.cloud[box_center_x][box_center_y][2]
         
         # Check if the function returned valid data and return the answer
-        if self.marker_center_x or self.marker_center_y or self.marker_depth is not None:
-            return True
-        else:
+        if math.isnan(self.marker_center_x) or math.isnan(self.marker_center_y) or math.isnan(self.marker_center_z):
             return False
+        else:
+            return True
 
     def create_pose_target(self, Ww, Wx, Wy, Wz, x, y, z):
         """
@@ -465,13 +424,15 @@ class Control:
 		"""
 		Function to find and manipulate an orange can
 		"""
-		
 		self.find_can_location()
 	
     def find_can_location(self):
 		"""
 		Function to search the RGB image for an orange can, and then return its co-ordinates
 		"""
+		
+		while raw_input("Enter: ") != "g":
+			waiting = None
 			
 		points_detected = False
 
@@ -481,14 +442,18 @@ class Control:
 			        points_detected = self.detect_calibration_marker()
 			    except:
 			        pass           
-		self.marker_depth	                
-		kinect_point = np.array([self.marker_center_x, self.marker_center_y, self.marker_depth])
+	                
+		kinect_point = np.array([self.marker_center_x, self.marker_center_y, self.marker_center_z])
 		
 		print kinect_point
-		x, y, z = self.calculate_translated_point(kinect_point)
-		print x 
-		print y
-		print z
+		new_B = self.calculate_translated_point(kinect_point)
+		
+		new_B = np.asarray(new_B)
+
+		x = new_B[0][0]
+		y = new_B[0][1]
+		z = new_B[0][2]
+
 		pose_target = self.create_pose_target(0.0977083761873,		# Ww
                                               0.714003795927,		# Wx
                                               -0.00449042997044,	# Wy
@@ -499,31 +464,23 @@ class Control:
 		self.right_arm.set_planner_id("RRTConnectkConfigDefault")
 		self.right_arm.set_pose_target(pose_target)
 		right_arm_plan = self.right_arm.plan()
-		self.right_arm.go()
+		
+self.right_arm.go()
 		
 
     def calculate_translated_point(self, K):
     	"""
     	Function to return a set of co-ordinates given Kinect co-ordinates and an affine transform
     	"""
+    	K = np.asmatrix(K)
     	n = len(K)
     	
-    	B = (self.R * K.T) + tile(self.t, (1, n))
+    	K2 = (self.R * K.T) + tile(self.t, (1, n))
     	B = K2.T
     	
-    	return float(B[0]), float(B[1]), floatB([2])
-    	
-    def command(self):
-    	while 1:
-    		comm = raw_input("Enter Command: ")
-    		
-    		if comm is "c":
-    			# Recalibrate robot
-    			self.calibrate_main()
-    			
-    		if comm is "m":
-    			# Move to object
-    			self.find_can_location()
+    	print 'Translated Point :'
+    	print B
+    	return B
 
 def main():
     """
@@ -536,8 +493,6 @@ def main():
 
     # Start the calibration process to link Baxter and Kinect Coordinates
     robot.calibrate_main()
-    
-    robot.command()
     
     # Search for orange can and move to that location
     robot.manipulate_orange_can()
